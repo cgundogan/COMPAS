783eb336cd772ccbced51b76897ee6517408bc65
Cenk Gündoğan <mail-github@cgundogan.de>
2017-05-12 09:03:22 +0200

    unit-test: use global dodags and reset them before every test

--- tests/unit-tests/routing/unit-tests.c
+++ tests/unit-tests/routing/unit-tests.c
@@ -13,35 +13,40 @@
 #include "compas/routing/pam.h"
 
 #define TEST_PREFIX_LEN (18)
+#define TEST_NAME_LEN (TEST_PREFIX_LEN + 2)
+
 static const char test_prefix[TEST_PREFIX_LEN] = "/HAW/t/te/tes/test";
+static const char test_name[TEST_NAME_LEN] = "/HAW/t/te/tes/test/1";
+
 static char buf_pam[sizeof(compas_pam_t) + TEST_PREFIX_LEN];
 static char buf_nam[sizeof(compas_nam_t) + 3 * sizeof(compas_tlv_t) + \
                     sizeof(uint16_t) + 2 * TEST_PREFIX_LEN];
 static const uint8_t face_addr[] = { 0x00, 0x01, 0x02, 0x03, \
                                      0x04, 0x05, 0x06, 0x07 };
+static compas_dodag_t dodag1, dodag2;
 
 void setUp(void) {
     memset(buf_pam, 0, sizeof(buf_pam));
     memset(buf_nam, 0, sizeof(buf_nam));
+    memset(&dodag1, 0, sizeof(dodag1));
+    memset(&dodag2, 0, sizeof(dodag2));
 }
 
 void tearDown(void) {}
 
 void test_compas_dodag_init_root(void)
 {
-    compas_dodag_t dodag;
-    compas_dodag_init_root(&dodag, test_prefix, TEST_PREFIX_LEN);
-    TEST_ASSERT_EQUAL_UINT16(dodag.rank, COMPAS_DODAG_ROOT_RANK);
-    TEST_ASSERT_EQUAL_UINT16(dodag.prefix.prefix_len, TEST_PREFIX_LEN);
-    TEST_ASSERT_EQUAL_STRING_LEN(dodag.prefix.prefix, test_prefix, TEST_PREFIX_LEN);
+    compas_dodag_init_root(&dodag1, test_prefix, TEST_PREFIX_LEN);
+    TEST_ASSERT_EQUAL_UINT16(dodag1.rank, COMPAS_DODAG_ROOT_RANK);
+    TEST_ASSERT_EQUAL_UINT16(dodag1.prefix.prefix_len, TEST_PREFIX_LEN);
+    TEST_ASSERT_EQUAL_STRING_LEN(dodag1.prefix.prefix, test_prefix, TEST_PREFIX_LEN);
 }
 
 void test_compas_pam_create(void)
 {
-    compas_dodag_t dodag;
-    compas_dodag_init_root(&dodag, test_prefix, TEST_PREFIX_LEN);
+    compas_dodag_init_root(&dodag1, test_prefix, TEST_PREFIX_LEN);
     compas_pam_t *pam = (compas_pam_t *) buf_pam;
-    compas_pam_create(&dodag, pam);
+    compas_pam_create(&dodag1, pam);
     TEST_ASSERT_EQUAL_UINT8(pam->type, COMPAS_MSG_TYPE_PAM);
     TEST_ASSERT_EQUAL_UINT16(pam->rank, COMPAS_DODAG_ROOT_RANK);
     TEST_ASSERT_EQUAL_UINT16(pam->prefix_len, TEST_PREFIX_LEN);
@@ -50,23 +55,20 @@ void test_compas_pam_create(void)
 
 void test_compas_pam_len(void)
 {
-    compas_dodag_t dodag;
-    compas_dodag_init_root(&dodag, test_prefix, TEST_PREFIX_LEN);
+    compas_dodag_init_root(&dodag1, test_prefix, TEST_PREFIX_LEN);
     compas_pam_t *pam = (compas_pam_t *) buf_pam;
-    size_t len = compas_pam_len(&dodag);
+    size_t len = compas_pam_len(&dodag1);
     TEST_ASSERT_EQUAL_UINT(len, TEST_PREFIX_LEN + sizeof(compas_pam_t));
 }
 
 void test_compas_pam_parse(void)
 {
-    compas_dodag_t dodag1;
-    compas_dodag_t dodag2;
     compas_dodag_init_root(&dodag1, test_prefix, TEST_PREFIX_LEN);
     compas_pam_t *pam = (compas_pam_t *) buf_pam;
     compas_pam_create(&dodag1, pam);
     uint8_t face_addr_len = sizeof(face_addr)/sizeof(face_addr[0]);
-    TEST_ASSERT_EQUAL_INT(1, compas_pam_parse(&dodag2, pam,
-                                              face_addr, face_addr_len));
+    TEST_ASSERT_EQUAL_INT(COMPAS_PAM_RET_CODE_NEWPARENT, compas_pam_parse(&dodag2, pam,
+                                                                          face_addr, face_addr_len));
     TEST_ASSERT_EQUAL_UINT16(dodag1.prefix.prefix_len, dodag2.prefix.prefix_len);
     TEST_ASSERT_EQUAL_STRING_LEN(dodag1.prefix.prefix, dodag2.prefix.prefix, dodag1.prefix.prefix_len);
     TEST_ASSERT_EQUAL_UINT16(dodag1.rank + 1, dodag2.rank);
@@ -98,14 +100,12 @@ void test_compas_pam_parse(void)
 
 void test_compas_dodag_parent_eq(void)
 {
-    compas_dodag_t dodag1;
-    compas_dodag_t dodag2;
     compas_dodag_init_root(&dodag1, test_prefix, TEST_PREFIX_LEN);
     compas_pam_t *pam = (compas_pam_t *) buf_pam;
     compas_pam_create(&dodag1, pam);
     uint8_t face_addr_len = sizeof(face_addr)/sizeof(face_addr[0]);
-    TEST_ASSERT_EQUAL_INT(1, compas_pam_parse(&dodag2, pam,
-                                              face_addr, face_addr_len));
+    TEST_ASSERT_EQUAL_INT(COMPAS_PAM_RET_CODE_NEWPARENT, compas_pam_parse(&dodag2, pam,
+                                                                          face_addr, face_addr_len));
     compas_face_t face;
     compas_face_init(&face, face_addr, face_addr_len);
     TEST_ASSERT_TRUE(compas_dodag_parent_eq(&dodag2, &face));
@@ -161,6 +161,22 @@ void test_compas_nam_tlv_iter(void)
     TEST_ASSERT_FALSE(compas_nam_tlv_iter(nam, &offset, &tlv));
 }
 
+void test_compas_nam_cache(void)
+{
+    compas_dodag_init_root(&dodag1, test_prefix, TEST_PREFIX_LEN);
+    compas_name_t name;
+    compas_name_init(&name, test_name, TEST_NAME_LEN);
+    compas_face_t face;
+    compas_face_init(&face, face_addr, sizeof(face_addr));
+    TEST_ASSERT_TRUE(compas_nam_cache_empty(&dodag1));
+    TEST_ASSERT_TRUE(compas_nam_cache_add(&dodag1, &name, &face));
+    TEST_ASSERT_FALSE(compas_nam_cache_empty(&dodag1));
+    compas_nam_cache_entry_t *found = compas_nam_cache_find(&dodag1, &name);
+    TEST_ASSERT_NOT_NULL(found);
+    TEST_ASSERT_TRUE(compas_nam_cache_del(&dodag1, &name));
+    TEST_ASSERT_TRUE(compas_nam_cache_empty(&dodag1));
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_compas_dodag_init_root);
@@ -172,5 +188,6 @@ int main(void) {
     RUN_TEST(test_compas_nam_tlv_add_name);
     RUN_TEST(test_compas_nam_tlv_add_lifetime);
     RUN_TEST(test_compas_nam_tlv_iter);
+    RUN_TEST(test_compas_nam_cache);
     return UNITY_END();
 }
